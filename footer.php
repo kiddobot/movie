a659aaad8fd90300ccdd63b2ea5ebb16b2dabe4f
Ravi Chaurasia <151345849+learnlead99@users.noreply.github.com>
2024-09-16 18:57:37 +0530

    movie comit

---
+++ footer.php
@@ -0,0 +1,51 @@
+ <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__logo">
+                            <a href="#"><img src="img/logo.png" alt=""></a>
+                        </div>
+                        <p>The customer is at the heart of our unique business model, which inculde online movie book show.</p>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Menu</h6>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="allmovie.php">All Movie</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="feedback.php">Feedback</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Category</h6>
+                        <ul>
+                            <li><a href="allmovie.php">Action</a></li>
+                            <li><a href="allmovie.php">Drama</a></li>
+                            <li><a href="allmovie.php">Comedy</a></li>
+                            <li><a href="allmovie.php">History</a></li>
+                            <li><a href="allmovie.php">Hrror</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>LOCATION</h6>
+                        <div class="footer__newslatter">
+                            <p>Inox Cinema, Near Clock Tower, 
+                                Dehradun-248001.
+                               
+</p>
+                            
+                       
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
